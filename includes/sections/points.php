49dc4ca3c5716b66d54e4d7352e89a2f50f3f1be
Petr Kaplunovich <yashikaren@gmail.com>
2016-05-23 21:14:40 -0400

    new point

--- includes/sections/points.php
+++ includes/sections/points.php
@@ -11,30 +11,36 @@
             </div>
 
             <div class='Point--green'>
-                <div class='Point__icon'><img src='/static/src/img/icons/icon_download.png' /></div>
+                <div class='Point__icon'><img src='/static/src/img/icons/icon_designer.png' /></div>
+                <h3>Designer Service</h3>
+                <p>Our 3D designers will make any of your customers dreams come true. Head of your customer on the Batman's body? Not a problem! <a class='Link-with-underline'>Read more</a></p>
+            </div>
+
+            <div class='Point--purple'>
+                <div class='Point__icon fb fb--jc-fs fb--ai-c'><img src='/static/src/img/icons/icon_download.png' /><div class='Point__icon__package'>Pro<span>only</span></div></div>
                 <h3>Downloadable 3D models</h3>
                 <p>You’ll be able to use the processed 3D model in any application as regular 3D model.</p>
             </div>
 
-            <div class='Point--purple'>
+            <div class='Point--blue'>
                 <div class='Point__icon'><img src='/static/src/img/icons/icon_storage.png' /></div>
                 <h3>Unlimited Storage</h3>
                 <p>All models that you decide to print will be synced with our cloud for free and stay there as long as you want.</p>
             </div>
 
-            <div class='Point--blue'>
+            <div class='Point--green'>
                 <div class='Point__icon'><img src='/static/src/img/icons/icon_support.png' /></div>
                 <h3>24 / 7 email support</h3>
                 <p>All models that you decide to print will be synced with our cloud for free and stay there as long as you want.</p>
             </div>
 
-            <div class='Point--green'>
+            <div class='Point--purple'>
                 <div class='Point__icon'><img src='/static/src/img/icons/icon_printing.png' /></div>
                 <h3>3D Printing automation</h3>
                 <p>Fully automated process of figurine creation: our designers will modify models however your customers wish and send them to the closest to you or your customer 3D printing company.</p>
             </div>
 
-            <div class='Point--purple'>
+            <div class='Point--blue'>
                 <div class='Point__icon'><img src='/static/src/img/icons/icon_branding.png' /></div>
                 <h3>Custom Branding</h3>
                 <p>All models can have your custom branding on them (check the examples in the Gallery).</p>
